2cea0adb2c76ad374fce0f9e371aa50c91e10767
fupengpeng <564055710@qq.com>
2017-11-08 09:39:06 +0800

    修复最后一次发表主题显示bug。

--- src/cn/itcast/oa/dao/impl/ForumDaoImpl.java
+++ src/cn/itcast/oa/dao/impl/ForumDaoImpl.java
@@ -33,28 +33,7 @@ public class ForumDaoImpl extends BaseDaoImpl<Forum> implements IForumDao {
 	@Override
 	public List<Forum> findAll() {
 		String hql = "FROM Forum f ORDER BY f.position";
-		System.out.println("---------------------");
-		List<Forum> list = this.getSession().createQuery(hql).list();
-		System.out.println("============================");
-		System.out.println("list = " + list.toString());
-		System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=");
-		for (Forum forum : list) {
-			System.out.println("1-1-1-1-1-1-1-1-1-1-1--1");
-			System.out.println("forum  == " + forum.toString());
-			System.out.println("4=4=4=4=4=4=4=4=4=4=4=4=4=4");
-			Set<Topic> topics = forum.getTopices();
-			System.out.println("7a7a7a7a7a7a7a7a7a7a7a7a7a7");
-			for (Topic topic : topics) {
-				System.out.println("f0f0f0f0f0f0f0f0f00f0f0f");
-				Reply reply = topic.getLastReply();
-				System.out.println("-f-f-f-f-f-f-f-f-f-f-f-f-f-");
-				System.out.println("reply  = " + reply.toString());
-				
-				System.out.println("topic  = " + topic.toString());
-			}
-			System.out.println("forum = " + forum.toString());
-		}
-		return list;
+		return this.getSession().createQuery(hql).list();
 	}
 	
 	
